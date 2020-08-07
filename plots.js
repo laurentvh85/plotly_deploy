a78e2a4363725475ecd0c7aed0b59d10fd9f73f5
Laurent Van Hassel <laurent.vanhassel@me.com>
2020-08-07 16:38:06 -0400

    without gauge

--- plots.js
+++ plots.js
@@ -8,11 +8,18 @@ function init() {
       selector
         .append("option")
         .text(sample)
-        .property("value", sample);
+        .property("value", sample)
     });
-})}
+      
+      // Load data on startup for 940
+      var startupId = sampleNames[0];
+      buildMetadata(startupId);
+      buildCharts(startupId);
+    }
+)};
 
 init();
+
 
 function optionChanged(newSample) {
   buildMetadata(newSample);
@@ -27,28 +34,65 @@ function buildMetadata(sample) {
     var PANEL = d3.select("#sample-metadata");
 
     PANEL.html("");
-    PANEL.append("h6").text("ID - " + result.id);
-    PANEL.append("h6").text("Ethnicity - " + result.ethnicity);
-    PANEL.append("h6").text("Gender - " + result.gender);
-    PANEL.append("h6").text("Age - " + result.age);
-    PANEL.append("h6").text("Location - " + result.location);
-    PANEL.append("h6").text("BBTYPE - " + result.bbtype);
-    PANEL.append("h6").text("WFREQ - " + result.wfreq);
-  
-  
-  
+    PANEL.append("h6").text(["id: " + result.id]);
+    PANEL.append("h6").text(["ethnicity: " + result.ethnicity]);
+    PANEL.append("h6").text(["gender: " + result.gender]);
+    PANEL.append("h6").text(["age: " + result.age]);
+    PANEL.append("h6").text(["location: " + result.location]);
+    PANEL.append("h6").text(["bbtype: " + result.bbtype]);
+    PANEL.append("h6").text(["wfreq: " + result.wfreq]);
   });
-  var trace = {
-    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    type: "bar"
-   };
-   var data = [trace];
-   var layout = {
-    title: "'Bar' Chart",
-    xaxis: { title: "Drinks"},
-    yaxis: { title: "% of Drinks Ordered"}
-   };
-   Plotly.newPlot("plotArea", data, layout);
+}
 
+
+
+//data for Charts
+function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+      var samples = data.samples;
+      var filter = samples.filter(object => object.id.toString() === sample);
+      var result = filter[0];
+      var otu_ids = result.otu_ids;
+      var otu_labels = result.otu_labels;
+      var sample_values = result.sample_values;
+
+     //Bar Chart
+      var trace = {
+          x: sample_values.slice(0, 10).reverse(),
+          y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+          text: otu_labels.slice(0, 10).reverse(),
+          type: "bar",
+          orientation: "h"
+      };
+      var data = [trace];
+      var layout = {
+         title: 'Top 10',
+         margin: { l: 100, r: 100, t: 100, b: 100 }
+      };
+      Plotly.newPlot("bar", data, layout);
+
+
+      // bubble chart
+
+      var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+          color: otu_ids,
+          size: sample_values
+        }
+      };
+      
+      var data = [trace1];
+      var layout = {
+        showlegend: false,
+        height: 600,
+        width: 1500
+      };
+      
+      Plotly.newPlot('bubble', data, layout);
+
+  });
 }
